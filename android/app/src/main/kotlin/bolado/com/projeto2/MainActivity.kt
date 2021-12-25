a52806ead9e8f0b338a72bc2d3498e7cf476b94e
Vinicius <vinnievandro@hotmail.com>
2021-12-25 15:01:29 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/bolado/com/projeto2/MainActivity.kt
@@ -0,0 +1,6 @@
+package bolado.com.projeto2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
