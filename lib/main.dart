9e8a8cd3e9194cb17c4b077fc5e22a0b34ec7708
Vinicius <vinnievandro@hotmail.com>
2021-12-26 16:24:17 -0300

    Projeto finalizado "partes de formularios para usos futuros"

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,159 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
-class homePage extends StatefulWidget {
-  const homePage({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  _homePageState createState() => _homePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _homePageState extends State<homePage> {
+class _HomeState extends State<Home> {
+  get raisedButtonStyle => null;
+
+  TextEditingController weigthController = TextEditingController();
+  TextEditingController heigthController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infotext = "informe seus dados!";
+
+  void _resetField() {
+
+    weigthController.text = "";
+    heigthController.text = "";
+
+    setState(() {
+      _infotext = "informe seus dados!";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weigth = double.parse(weigthController.text);
+      double heigth = double.parse(heigthController.text) / 100;
+      double imc = weigth / (heigth * heigth);
+      print(imc);
+      if (imc < 18.6) {
+        _infotext = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infotext = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infotext = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infotext = "Obesidade grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infotext = "Obesidade grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40) {
+        _infotext = "Obesidade grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _resetField,
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.person_outline, size: 120, color: Colors.green),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Peso (kg)',
+                    labelStyle: TextStyle(
+                      color: Colors.red,
+                      fontSize: 40,
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 25,
+                  ),
+                  controller: weigthController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'insira seu peso';
+                    }
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Altura (cm)',
+                    labelStyle: TextStyle(
+                      color: Colors.red,
+                      fontSize: 40,
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 25,
+                  ),
+                  controller: heigthController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'insira sua Altura';
+                    }
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: Container(
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _calculate();
+                        }
+                      },
+                      child: Text(
+                        "calcular",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  _infotext,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            )),
+      ),
+    );
   }
 }
-
-
